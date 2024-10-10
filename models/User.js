2d39975e86d587edfb5b1ab73f8b972b3e76ab08
Amanda Lang <Cadenc2003@icloud.com>
2024-10-09 23:20:10 -0500

    set up file structure and schemas

---
+++ models/User.js
@@ -0,0 +1,34 @@
+const { Schema, model } = require('mongoose')
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            isUnique: true,
+            required: true,
+            trimmed: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            isUnique: true,
+            verifyIsEmail,
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'thought',
+            },
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'user',
+            },
+        ],
+    }
+)
+
+const User = model('user', userSchema)
+
+module.exports = User
